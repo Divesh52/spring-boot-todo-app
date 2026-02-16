c9cbc73f359e2b7b6fb07e910d59e746d387f1fa
Divesh Koul <diveshkoul5@gmail.com>
2026-02-16 18:08:52 +0530

    Update TaskController.java

--- src/main/java/com/app/todoapp/controller/TaskController.java
+++ src/main/java/com/app/todoapp/controller/TaskController.java
@@ -22,7 +22,7 @@ public class TaskController {
     public String getTasks(Model model) {
         List<Task> tasks = taskService.getAllTasks();
         model.addAttribute("tasks", tasks);
-        return "index";   // Make sure your file is index.html
+        return "task";   // Make sure your file is task.html
     }
 
     @PostMapping("/")
